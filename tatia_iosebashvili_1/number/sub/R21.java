c9cc71e88936841a5f9e80f3268b7f08a28606bd
ambivert.t <151209994+tataios@users.noreply.github.com>
2024-03-25 12:16:04 +0400

    quiz2

--- tatia_iosebashvili_1/number/sub/R21.java
+++ tatia_iosebashvili_1/number/sub/R21.java
@@ -7,7 +7,7 @@ import quizes.q2.tatia_iosebashvili_1.cow.A22;
 public class R21 {
     public void printAreaOfRectangle(){
         A21 area = new A21();
-        System.out.println(area);
+        System.out.println("area= " + (A21.len * A21.hei));
 
 
     }
